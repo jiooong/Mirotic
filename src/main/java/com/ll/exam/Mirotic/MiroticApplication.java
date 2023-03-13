ec42dbb5363aecff6500728f197e4dca3708b46b
jiooong <lucy991010@gmail.com>
2023-03-13 16:41:29 +0900

    Chore : init Start Spring

---
+++ src/main/java/com/ll/exam/Mirotic/MiroticApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.Mirotic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiroticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiroticApplication.class, args);
+	}
+
+}
